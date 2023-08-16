265f3f1152befee8b17fe774133b061fd06e90f3
larapdalese <126460907+larapdalese@users.noreply.github.com>
2023-08-16 08:20:15 -0300

    Create exercicioum.py

---
+++ exercicioum.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def dever_rafa():
+    rafael = np.arange(1, 11)
+    rafaell = np.arange(11, 21)
+    
+    soma_elementos = rafael + rafaell
+    
+    print("rafael:", rafael)
+    print("rafaell:", rafaell)
+    print("Soma Elemento a Elemento:", soma_elementos)
+
+# Chamando a função para executar as operações
+dever_rafa()
